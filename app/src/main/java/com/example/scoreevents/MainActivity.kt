4307c87d6a774cceeb0c570e5d3b45392990eed0
Mariam Zahid <120362767+MariamZahi@users.noreply.github.com>
2023-07-10 02:57:19 -0400

    import widgets

---
+++ app/src/main/java/com/example/scoreevents/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.example.scoreevents
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
